b300e2718af6812d30042c99bf1c830812e2f53b
Michael Wahlberg <mike.wahlberg@gmail.com>
2016-04-12 13:59:57 -0400

    Add scripts and modify GTAapplication

---
+++ src/scripts/form_submit_GTA.php
@@ -0,0 +1,6 @@
+<?
+  //Connect to SQL DB
+  $connect = mysqli_connect("my host", "my user", "my password", "my db");
+  //Send data to DB
+  mysqli_query($connect, "INSERT INTO _____ () VALUES ()");
+?>
