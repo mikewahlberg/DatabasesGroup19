d675aaef5817905331d9fc6761e30f3823ca3c8e
dmw62274 <dmw62274@gmail.com>
2016-04-13 17:20:49 -0400

    Update form_submit_login.php

--- src/scripts/form_submit_login.php
+++ src/scripts/form_submit_login.php
@@ -1,6 +1,11 @@
 <?
   //Connect to SQL DB
-  $connect = mysqli_connect("my host", "my user", "my password", "my db");
+  $link = mysql_connect("my host", "my user", "my password", "my db");
+  if (!$link){
+    die('could not connect:' . mysql_error());
+  }
   //Send data to DB
-  mysqli_query($connect, "INSERT INTO _____ () VALUES ()");
+  mysqli_query($link, "INSERT INTO APPLICANT(PID, stu_first_name, stu_last_name, stu_email, stu_phone, PHD_CS, semesters_grad, 
+  SPEAK, semesters_GTA, GPA) VALUES (GTAPID, GTAname, GTAlname, GTAemail, GTAphone, phdChoice, GTAsemestercount, speakChoice,
+  GTAworkingsemestercount, totalGPA)");
 ?>
